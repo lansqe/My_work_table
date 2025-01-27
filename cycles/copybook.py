4c0c6f69f07d41ce6edd33090157070156176d85
lncru <pisarev.orig@mail.ru>
2025-01-27 13:02:00 +0300

    complete

--- cycles/copybook.py
+++ cycles/copybook.py
@@ -20,18 +20,18 @@
 #
 # print('это простое число' if count == 2 else 'это не простое число')
 
-numbers = [16, 32, 1, 7, -5, 256, 0, 1.3333, 15.5]
-
-for number in numbers:
-    if number < 1:
-        is_two_power = False
-    elif number == 1:
-        is_two_power = True
-    else:
-        while number % 2 == 0:
-            number //= 2
-        is_two_power = number == 1
-    print(is_two_power)
+# numbers = [16, 32, 1, 7, -5, 256, 0, 1.3333, 15.5]
+#
+# for number in numbers:
+#     if number < 1:
+#         is_two_power = False
+#     elif number == 1:
+#         is_two_power = True
+#     else:
+#         while number % 2 == 0:
+#             number //= 2
+#         is_two_power = number == 1
+#     print(is_two_power)
 
 
 
@@ -43,27 +43,37 @@ for number in numbers:
 # 1 посетитель = один напиток
 
 
-coffee = 0.2 * 1000
-milk = 3.0 * 1000
+coffee_on_gram = 70
+milk_on_gram = 100
 
-num_dink = 0
-def milk_remaining(milk):
+num_drink = 0
+visitors = 0
 
 
-while milk > 0 and coffee > 0:
-    if num_dink == 3 or num_dink % 3 == 0 and num_dink % 5 == 0:
+def milk_remaining(milk: float, name: str) -> float:
+    if name == 'латте':
+        milk -= 180
+        print('Был заказан латте')
+    elif name == 'капучино':
+        milk -= 100
+        print('Был заказан капучино')
+    else:
+        print('Был заказан американо')
+    return milk
+
+
+while milk_on_gram > 0 and coffee_on_gram > 0:
+    if num_drink == 3 or num_drink % 15 == 0:
         name_drink = 'капучино'
-    elif num_dink == 5:
+    elif num_drink == 5:
         name_drink = 'латте'
     else:
-        name_dink = 'американо'
-    num_dink += 1
-    coffee -= 0.007
-    if num
-    milk -= 180 if name_drink == 'латте' else 100
+        name_drink = 'американо'
+    num_drink += 1
+    coffee_on_gram -= 7
+    milk_remaining(milk_on_gram, name_drink)
+
+print(f'Осталось {milk_on_gram} мл. молока и {coffee_on_gram} гр. кофе')
+print(f'Посетителей пришло за смену: {num_drink}')
 
 
-print(coffee, milk)
-
-
-visitors = 0
