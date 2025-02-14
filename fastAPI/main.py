a9fd65fe9d697ca32d2d97ef9110749379c48a64
lncru <pisarev.orig@mail.ru>
2025-02-14 13:02:03 +0300

    fastapi 2.3

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from models import User
+from models import User, Feedback
 
 app = FastAPI()
-
+data_feedback = []
 
 @app.get('/')
 async def root():
@@ -22,3 +22,17 @@ async def adult(user: User):
     "age": user.age,
     "is_adult": is_adult
 }
+
+
+@app.post('/feedback')
+async def users_feedback(f: Feedback):
+    data_feedback.append({'name': f.name, 'comments': f.message})
+    return {
+        'message': f"Feedback received. Thank you, {f.name}!"
+    }
+
+
+@app.get('/comments')
+async def show_feedback():
+    return data_feedback
+
--- fastAPI/models.py
+++ fastAPI/models.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class User(BaseModel):
     name: str
     age: int
+
+
+class Feedback(BaseModel):
+    name: str
+    message: str
