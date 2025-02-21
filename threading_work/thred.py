7a33c4daeff143a30229c696092f1edcbe890b7b
lncru <pisarev.orig@mail.ru>
2025-02-21 21:45:50 +0300

    enum test

--- threading_work/thred.py
+++ threading_work/thred.py
@@ -1,27 +1,7 @@
-import asyncio
+import threading
 
 
-async def fun1(x):
-    print(x**2)
-    await asyncio.sleep(3)
-    print('fun1 завершена')
+t='anagram'
+c='nagaram'
 
-
-async def fun2(x):
-    print(x**0.5)
-    await asyncio.sleep(3)
-    print('fun2 завершена')
-
-
-async def main():
-    task1 = asyncio.create_task(fun1(4))
-    task2 = asyncio.create_task(fun2(4))
-
-    print(type(task1))
-    print(task1.__class__.__bases__)
-
-    await task1
-    await task2
-
-
-asyncio.run(main())
+print(sorted(t))
